7421348b7b9ff0e5129fb91c971f70feb86868a3
masoumeh masoumi <masoumehmasouminia@gmail.com>
2023-02-21 00:43:28 +0330

    add pro to git

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,23 +1,66 @@
-import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+import React, { useEffect } from 'react';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import '../App.css';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const handleForm = (e: any) => {
-        e.preventDefault(e);
-        alert('Welcome');
-    }
 
     return (
-        <form className='Form' onSubmit={handleForm}>
-            <h1>Login</h1>
-            <p>Please sign in to continue</p>
-            <div className='input'>
-                <input type="text" placeholder='EMAIL' />
-                <input type="text" placeholder='PASSWORD' />
 
-                <button className='button-S'>SIGN UP </button>
-                <button className='button-L'>LOGIN </button>
-            </div>
-        </form>
+        <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{ remember: true }}
+        // onFinish={onFinish}
+        >
+            <h2>Welcome back</h2>
+            <Form.Item
+                name="username"
+                rules={[{ required: true, message: 'Please input your Username!' }]}
+            >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />}
+                    placeholder="Username"
+                />
+            </Form.Item>
+
+            <Form.Item
+                name="password"
+                rules={[{ required: true, message: 'Please input your Password!' }]}
+            >
+                <Input.Password
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                />
+            </Form.Item>
+
+            <Form.Item>
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <a className="login-form-forgot" href="">
+                    Forgot password?
+                </a>
+            </Form.Item>
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    Log in
+                </Button>
+            </Form.Item>
+            Or <Link to="/Signup">Register now!</Link>
+        </Form>
+
+
+
+
+
+
+
+
+
     );
 };
 export default Login;
--- src/Pages/Signup.tsx
+++ src/Pages/Signup.tsx
@@ -1,33 +1,189 @@
+import { Button, Checkbox, Form, Input } from 'antd';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css';
-
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
 
 const Signup = () => {
-
-    const handleForm = (e: any) => {
-        e.preventDefault(e);
-        alert('Welcome')
-    }
     return (
-        <form className='Form' onSubmit={handleForm}>
-            <h1>Create Account</h1>
-            <div className='input'>
-                <input type="text" placeholder='FULL NAME' />
-                <input type="text" placeholder='EMAIL' />
-                <input type="text" placeholder='PASSWORD' />
-                <input type="text" placeholder='CONFIRM PASSWORD' />
+
+        <Form
+            name="register"
+            className='login-form'
+
+        // {...formItemLayout}
+        // form={form}
+
+        // onFinish={onFinish}
+        // initialValues={{ residence: ['zhejiang', 'hangzhou', 'xihu'], prefix: '86' }}
+        // style={{ maxWidth: 600 }}
+        // scrollToFirstError
+        >
+            <h2>Sign up</h2>
+            <Form.Item
+                name="Fullname"
+                // label="Full Name"
+                rules={[
+                    {
+                        type: 'string',
+
+                    },
+                    {
+                        required: true,
+                        message: 'Please input your Full name!',
+                    },
+                ]}
+            >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />}
+                    placeholder="Username"
+                />
+            </Form.Item>
+            <Form.Item
+                name="email"
+                // label="E-mail"
+                rules={[
+                    {
+                        type: 'email',
+                        message: 'The input is not valid E-mail!',
+                    },
+                    {
+                        required: true,
+                        message: 'Please input your E-mail!',
+                    },
+                ]}
+            >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />}
+                    placeholder="E-mail"
+                />
+            </Form.Item>
+            <Form.Item
+                name="phone"
+                // label="Phone Number"
+                rules={[
+                    {
+                        type: 'number',
+                        message: 'The input is not valid phone number!',
+                    },
+                    {
+                        required: true,
+                        message: 'Please input your phone number!',
+                    },
+                ]}
+            >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />}
+                    placeholder="Phone Number"
+                />
+            </Form.Item>
+
+            <Form.Item
+                name="password"
+                // label="Password"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your password!',
+                    },
+                ]}
+                hasFeedback
+            >
+                <Input.Password
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                />
+            </Form.Item>
+
+            <Form.Item
+                name="confirm"
+                // label="Confirm Password"
+                dependencies={['password']}
+                hasFeedback
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please confirm your password!',
+                    },
+                    ({ getFieldValue }) => ({
+                        validator(_, value) {
+                            if (!value || getFieldValue('password') === value) {
+                                return Promise.resolve();
+                            }
+                            return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                        },
+                    }),
+                ]}
+            >
+                <Input.Password
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+            </Form.Item>
 
 
-            </div>
-            <input type="checkbox" className='checkbox'/>
-            <label >
-                قوانین را خواندم و قبول دارم
-            </label>
 
-            {/* <button className='button-S'>SIGN UP </button>
-            <button className='button-L'>LOGIN </button> */}
+            <Form.Item
+                name="agreement"
+                valuePropName="checked"
+                rules={[
+                    {
+                        validator: (_, value) =>
+                            value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+                    },
+                ]}
+            // {...tailFormItemLayout}
+            >
+                <Checkbox>
+                    I have read the <a href="">agreement</a>
+                </Checkbox>
+            </Form.Item>
 
-        </form>
+            <Form.Item >
+                {/* {...tailFormItemLayout}> */}
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    Register
+                </Button>
+            </Form.Item>
+            Already have an account? <Link to="/Login">Login now!</Link>
+        </Form>
+
+
+
+
+
+
+
+
+
+
+        // const handleForm = (e: any) => {
+        //     e.preventDefault(e);
+        //     alert('Welcome')
+        // }
+        // 
+        //     <form className='Form' onSubmit={handleForm}>
+        //         <h1>Create Account</h1>
+        //         <div className='input'>
+        //             <input type="text" placeholder='FULL NAME' />
+        //             <input type="email" placeholder='EMAIL' />
+        //             <input type="number" placeholder='PHONE NUMBER' />
+        //             <input type="password" placeholder='PASSWORD' />
+        //             <input type="password" placeholder='CONFIRM PASSWORD' />
+        //         </div>
+        //         <div>
+        //             <input type="checkbox" className='checkbox' />
+        //             <label className='label'>
+        //                 قوانین را خواندم و قبول دارم
+        //             </label>
+        //         </div>
+        //         <div className='button-div'>
+        //             <button className='button-S'>SIGN UP </button>
+        //             <button className='button-L'>LOGIN </button>
+
+        //         </div>
+
+
+        //     </form>
     );
 };
 export default Signup;
